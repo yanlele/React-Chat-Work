11cf6e7d875080311b286eb27b33b34a53d4dac6
yanlele <331393627@qq.com>
2017-12-02 22:53:22 +0800

    如果子组件只有一个render函数，可以直接定义一个function来作为子组件，通过参数来传递数据，不过function的名字一定要大写。

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends React.Component {
             <div>
                 <h2>独立团, 团长{boss}</h2>
                 <OneCampsite boss="嗷大喵"></OneCampsite>
+                <Cavalry boss="奶萌兔"></Cavalry>
             </div>
         )
     }
@@ -21,6 +22,13 @@ class OneCampsite extends React.Component{
             <h2>一营营长，{this.props.boss}</h2>
         )
     }
+}
+
+//如果只有一个render方法的calss 可以直接写成一个function，父子通信可以通过参数的形式来实现，但是首字母一定要大写
+function Cavalry(props){
+    return (
+        <h2>骑兵连连长 {props.boss} , 冲锋！</h2>
+    )
 }
 
 export  default App;
