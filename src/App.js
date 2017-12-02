b53278eb10bbae956495558c38a7b036f5a55075
yanlele <331393627@qq.com>
2017-12-02 22:48:45 +0800

    通过props，父子组件传递信息

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ class App extends React.Component {
         return (
             <div>
                 <h2>独立团, 团长{boss}</h2>
-                <OneCampsite></OneCampsite>
+                <OneCampsite boss="嗷大喵"></OneCampsite>
             </div>
         )
     }
 }
 
 class OneCampsite extends React.Component{
+
+
     render(){
-        const boss='张大彪';
+        // const boss='张大彪';
         return(
-            <h2>一营营长，{boss}</h2>
+            <h2>一营营长，{this.props.boss}</h2>
         )
     }
 }
