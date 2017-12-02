df54aea4c70d9082b427ae41a2fa671f89168d58
yanlele <331393627@qq.com>
2017-12-02 23:21:41 +0800

    解决this指针问题的终极解决方案

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 class App extends React.Component {
     render() {
-        const boss='李云龙';
+        const boss = '李云龙';
         return (
             <div>
                 <h2>独立团, 团长{boss}</h2>
@@ -13,31 +13,31 @@ class App extends React.Component {
     }
 }
 
-class OneCampsite extends React.Component{
-    constructor(props){
+class OneCampsite extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={
-            solders:['虎子','大王','李狗蛋']
+        this.state = {
+            solders: ['虎子', '大王', '李狗蛋']
         }
 
         // this.addSolder=this.addSolder.bind(this);
     }
 
-    addSolder(){
+    addSolder() {
         console.log('hell add solder');
         this.setState({
-            solders:[...this.state.solders,'新兵蛋子'+Math.random()]
+            solders: [...this.state.solders, '新兵蛋子' + Math.random()]
         })
     }
 
-    render(){
+    render() {
         // const boss='张大彪';
-        return(
+        return (
             <div>
                 <h2>一营营长，{this.props.boss}</h2>
-                <button onClick={()=>this.addSolder()}>新兵入伍</button>
+                <button onClick={this.addSolder.bind(this)}>新兵入伍</button>
                 <ul>
-                    {this.state.solders.map((value)=>{
+                    {this.state.solders.map((value) => {
                         return <li key={value}>{value}</li>
                     })}
                 </ul>
@@ -47,7 +47,7 @@ class OneCampsite extends React.Component{
 }
 
 //如果只有一个render方法的calss 可以直接写成一个function，父子通信可以通过参数的形式来实现，但是首字母一定要大写
-function Cavalry(props){
+function Cavalry(props) {
 
     return (
         <h2>骑兵连连长 {props.boss} , 冲锋！</h2>
