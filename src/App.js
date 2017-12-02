e453ac8d781bc7531943761087d9e6db564ecdcc
yanlele <331393627@qq.com>
2017-12-02 22:46:40 +0800

    多个组件嵌套

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import React from 'react'
 
 class App extends React.Component {
     render() {
+        const boss='李云龙';
         return (
-            <h2>独立团</h2>
+            <div>
+                <h2>独立团, 团长{boss}</h2>
+                <OneCampsite></OneCampsite>
+            </div>
+        )
+    }
+}
+
+class OneCampsite extends React.Component{
+    render(){
+        const boss='张大彪';
+        return(
+            <h2>一营营长，{boss}</h2>
         )
     }
 }
