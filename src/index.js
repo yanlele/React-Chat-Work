1c5224dc93e4c14461e816da4088f8f072a84a2a
yanlele <331393627@qq.com>
2017-12-03 21:27:32 +0800

    redux的基本时间管理

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//通过reducer建立
+//根据老的状态state和action生成新的state
+function counter(state = 0, action) {
+    switch (action.type) {
+        case 'add':
+            return state + 1;
+        case 'subtract':
+            return state - 1;
+        default:
+            return 10;
+    }
+}
+
+
+//1、新建store
+const store = createStore(counter);
+
+const init =store.getState();
+console.log(init);
+
+//派发事件 传递action
+store.dispatch({
+    type:'add'
+});
+console.log(store.getState());
+store.dispatch({
+    type:'add'
+});
+console.log(store.getState());
+store.dispatch({
+    type:'subtract'
+});
+console.log(store.getState());
