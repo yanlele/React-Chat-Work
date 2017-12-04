cf009b01c5dd3d93053d31e7d68222bda8fcc955
yanlele <331393627@qq.com>
2017-12-04 08:30:26 +0800

    在谷歌浏览器添加redux-Devtools，然后我们在程序中就可以添加window.devToolsExtension

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import thunk from 'redux-thunk'//这个可以让我们的redux拥有处理异步的能力
 import App from './App'
-import {createStore,applyMiddleware} from 'redux'
+import {createStore,applyMiddleware,compose} from 'redux'
 import {counter,addGun,removeGun ,addGunAsync} from './index.redux'
 
-const store=createStore(counter,applyMiddleware(thunk));
+const reduxDevtools=window.devToolsExtension?window.devToolsExtension():()=>{};
+const store=createStore(counter,compose(
+    applyMiddleware(thunk),
+    reduxDevtools
+));
 
 function render(){
     ReactDom.render(
