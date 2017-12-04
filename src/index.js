d984e5c02770a10d7707fbb8a88d18ef08b726f4
yanlele <331393627@qq.com>
2017-12-04 09:21:24 +0800

    最简单的一个react-router4的一个实例

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDom from 'react-dom';
 import thunk from 'redux-thunk'//这个可以让我们的redux拥有处理异步的能力
 import {Provider} from 'react-redux'
 import App from './App'
+import {BrowserRouter, Route, Link} from 'react-router-dom'
+
 import {createStore, applyMiddleware, compose} from 'redux'
 import {counter} from './index.redux'
 
@@ -13,10 +15,35 @@ const store = createStore(counter, compose(
     reduxDevtools
 ));
 
+function Erying() {
+    return <h2>二营</h2>
+}
+
+function Cavalry(){
+    return <h2>骑兵连</h2>
+}
 
 ReactDom.render(
     ( <Provider store={store}>
-        <App  />
+        <BrowserRouter>
+            <div>
+                <ul>
+                    <li>
+                        <Link to='/'>一营</Link>
+                    </li>
+                    <li>
+                        <Link to='/erying'>二营</Link>
+                    </li>
+                    <li>
+                        <Link to='/cavalry'>骑兵连</Link>
+                    </li>
+                </ul>
+                {/*exact可以让我们的路由完全匹配*/}
+                <Route path='/' exact component={App}/>
+                <Route path='/erying' component={Erying}/>
+                <Route path='/cavalry' component={Cavalry}/>
+            </div>
+        </BrowserRouter>
     </Provider> ),
     document.getElementById('root')
 )
