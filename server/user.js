47b3082f08aa66ee8024acea4f0a7d686461f868
yanlele <331393627@qq.com>
2017-12-10 20:58:18 +0800

    通过cookie来存储用户信息

--- server/user.js
+++ server/user.js
@@ -4,6 +4,13 @@ const router=express.Router();
 const model=require('./model');
 const User=model.getModel('user');
 
+//定义统一的查询条件，过滤掉我们不需要的数据
+const _filter={
+    pwd:0,
+    __v:0
+};
+
+//给后端调试用的一个接口
 router.get('/list',function(req,res){
     // User.remove({});
    User.find({},function(err,doc){
@@ -14,13 +21,16 @@ router.get('/list',function(req,res){
 //登录功能
 router.post('/login',function(req,res){
     const {user,pwd}=req.body;
-    User.findOne({user,pwd:md5Pwd(pwd)},{'pwd':0},function(err,doc){
+    User.findOne({user,pwd:md5Pwd(pwd)},_filter,function(err,doc){
         if(!doc){
             res.status(500).json({
                 code:1,
                 msg:'用户名或者密码错误'
             })
         }
+
+        res.cookie('userid',doc._id);
+
         res.status(200).json({
             code:0,
             data:doc
@@ -58,11 +68,31 @@ router.post('/register',function(req,res){
     })
 });
 
+//验证用户是否登录
 router.get('/info',function(req,res){
-    //用户有没有cookie
-    res.json({
-        code:1
+    const {userid}=req.cookies;
+    if(!userid){
+        //用户有没有cookie
+        res.json({
+            code:1
+        })
+    }
+
+    User.findOne({_id:userid},_filter,function(err,doc){
+        if(err){
+            res.status(500).json({
+                code:1,
+                msg:'系统错误'
+            })
+        }
+        if(doc){
+            res.status(200).json({
+                code:0,
+                data:doc
+            })
+        }
     })
+
 });
 
 function md5Pwd(pwd){
