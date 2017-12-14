e7f49a3da7db06896f962c5779d4487d0b0c7811
yanlele <331393627@qq.com>
2017-12-14 20:42:30 +0800

    学习进度8-2

--- src/component/dashboard/dashboard.js
+++ src/component/dashboard/dashboard.js
@@ -5,8 +5,7 @@ class Dashboard extends React.Component {
         return (
             <div>
                 <h2>Dashboard</h2>
-                <Route path="/boss" component={Boss}/>
-                <Route path="/boss" component={Genius}/>
+
                 <h2>footer</h2>
             </div>
         )
