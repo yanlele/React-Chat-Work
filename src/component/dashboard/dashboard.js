e070f07b627ae0122bae766970f7fc81877c9e68
yanlele <331393627@qq.com>
2017-12-16 23:54:19 +0800

    dashboard组件以及navlink组件定义完成

--- src/component/dashboard/dashboard.js
+++ src/component/dashboard/dashboard.js
@@ -1,12 +1,77 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {NavBar} from 'antd-mobile'
+import {withRouter} from 'react-router-dom'
+import NavLinkBar from '../navlink/navlink'
 
+function Boss(){
+    return <div>Boss首页</div>
+}
+
+function Genius(){
+    return <div>Genius首页</div>
+}
+
+function Msg(){
+    return <div>消息首页</div>
+}
+
+function User(){
+    return <div>User个人中心首页</div>
+}
+
+
+@connect(
+    state=>state
+)
 class Dashboard extends React.Component {
+
     render() {
+        const user=this.props.user;
+        const {pathname}=this.props.location;
+        console.log(pathname);
+        const navList = [
+            {
+                path:'/boss',
+                text:'牛人',
+                icon:'boss',
+                title:'牛人列表',
+                component:Boss,
+                hide:user.type==='genius'
+            },
+            {
+                path:'/genius',
+                text:'boss',
+                icon:'job',
+                title:'BOSS列表',
+                component:Genius,
+                hide:user.type==='boss'
+            },
+            {
+                path:'/msg',
+                text:'消息',
+                icon:'msg',
+                title:'消息列表',
+                component:Msg
+            },
+            {
+                path:'/me',
+                text:'我',
+                icon:'user',
+                title:'个人中心',
+                component:User
+            }
+        ]
+
         return (
             <div>
-                <h2>Dashboard</h2>
+                <NavBar
+                    mode="dard"
+                >
+                    {navList.find(v=>v.path===pathname).title}
+                </NavBar>
 
-                <h2>footer</h2>
+                <NavLinkBar data={navList}/>
             </div>
         )
     }
