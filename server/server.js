54aec611756b896d50915b73862b300be1c87f85
yanlele <331393627@qq.com>
2017-12-17 16:10:12 +0800

    实现前后实时显示通信正在进行中

--- server/server.js
+++ server/server.js
@@ -2,13 +2,24 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 
+const app = express()
+
+//socketio用于express
+const server=require('http').Server(app);
+const io=require('socket.io')(server);
+
+io.on('connection',function(socket){
+	console.log(`user is login`)
+});
+
+
 const userRouter = require('./user')
 
-const app = express()
+
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use('/user',userRouter)
-app.listen(8082,function(){
+server.listen(8082,function(){
 	console.log('Node app start at port 8082')
 })
 
--- src/component/chat/chat.js
+++ src/component/chat/chat.js
@@ -1,12 +1,39 @@
 import React from 'react'
+import {List,InputItem} from 'antd-mobile'
+import io from 'socket.io-client'
 
 class Chat extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={text:''}
+    }
+
+    componentDidMount(){
+        const socket=io('ws://localhost:8082');
+    }
+
+    handleSubmit(){
+        console.log(this.state);
+    }
+
+
     render(){
         console.log(this.props)
 
         return (
-            <div>
-                <h2>chat with user:{this.props.match.params.user}</h2>
+            <div className="stick-footer">
+                <List>
+                    <InputItem
+                        placeholder="请输入"
+                        value={this.state.text}
+                        onChange={v=>{
+                            this.setState({text:v})
+                        }}
+                        extra={<span onClick={()=>{this.handleSubmit()}}>发送</span>}
+                    >
+                        信息
+                    </InputItem>
+                </List>
             </div>
         )
     }
