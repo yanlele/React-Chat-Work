c6db6ccd62aa009580deaaa351be5ec11f4aacf3
yanlele <331393627@qq.com>
2017-12-24 21:49:21 +0800

    redux基础知识点儿

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 #### react16基础语法使用
-- [基础知识点](./book/1、基础知识.md)
+- [基础语法知识点](./book/1、基础知识.md)
 
 #### redux
-
+- [redux基础知识点](./book/2、redux基础知识点.md)
 
 #### react-router4
 
--- src/App.js
+++ src/App.js
@@ -1,74 +1,23 @@
 import React from 'react'
-import {Button, List} from 'antd-mobile'
+import {connect} from 'react-redux'
+import {addGun, removeGun, addGunAsync} from './index.redux'
 
+// 装饰器模式
+@connect(
+    state => ({num: state}),
+    {addGun, removeGun, addGunAsync}
+)
 class App extends React.Component {
     render() {
-        const boss = '李云龙';
+        // num addGun，removeGun，addGunAsync都是connect给的，不需要手动dispatch
         return (
             <div>
-                <h2>独立团 , 团长{boss}</h2>
-                <OneCamp boss="张大喵"/>
-                <Cavalry boss="嗷大喵"/>
+                <h2>现在有机枪{this.props.num}把</h2>
+                <button onClick={this.props.addGun}>申请武器</button>
+                <button onClick={this.props.removeGun}>上交武器</button>
+                <button onClick={this.props.addGunAsync}>拖两天再给</button>
             </div>
         )
     }
 }
-
-function Cavalry(props) {
-    return (
-        <div>
-            <h2>骑兵连连长{props.boss}</h2>
-        </div>
-    )
-}
-
-//创建一个一营组件
-class OneCamp extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            solders: ['张大彪', '二愣子', '李狗蛋']
-        }
-    }
-
-    componentWillMount() {
-        console.log('组件马上就要加载了')
-    }
-
-    componentDidMount() {
-        console.log('组件加载完毕')
-    }
-
-    addSolder() {
-        console.log(1);
-        this.setState({
-            solders: [...this.state.solders, '新兵蛋子' + Math.random()]
-        })
-    }
-
-    render() {
-        console.log('组件正在加载');
-        return (
-            <div>
-                <h2>一营营长 - {this.props.boss}</h2>
-                <Button type="primary" onClick={this.addSolder.bind(this)}>新兵入伍</Button>
-
-                <List
-                    renderHeader='士兵列表'
-                >
-                    {this.state.solders.map(v=>{
-                        return (
-                            <List.Item
-                                key={v}
-                            >
-                                {v}
-                            </List.Item>
-                        )
-                    })}
-                </List>
-            </div>
-        )
-    }
-}
-
 export default App;
