295847d0f7376065a4c5f7589a40fca9251c140a
yanlele <331393627@qq.com>
2017-12-24 20:08:52 +0800

    antd-mobile的使用

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import {Button, List} from 'antd-mobile'
 
-class App extends React.Component{
-    render(){
-        const boss='李云龙';
+class App extends React.Component {
+    render() {
+        const boss = '李云龙';
         return (
             <div>
                 <h2>独立团 , 团长{boss}</h2>
@@ -13,7 +14,7 @@ class App extends React.Component{
     }
 }
 
-function Cavalry(props){
+function Cavalry(props) {
     return (
         <div>
             <h2>骑兵连连长{props.boss}</h2>
@@ -22,40 +23,49 @@ function Cavalry(props){
 }
 
 //创建一个一营组件
-class OneCamp extends React.Component{
-    constructor(props){
+class OneCamp extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={
-            solders:['张大彪','二愣子','李狗蛋']
+        this.state = {
+            solders: ['张大彪', '二愣子', '李狗蛋']
         }
     }
 
-    componentWillMount(){
+    componentWillMount() {
         console.log('组件马上就要加载了')
     }
 
-    componentDidMount(){
+    componentDidMount() {
         console.log('组件加载完毕')
     }
 
-    addSolder(){
+    addSolder() {
         console.log(1);
         this.setState({
-            solders:[...this.state.solders,'新兵蛋子'+Math.random()]
+            solders: [...this.state.solders, '新兵蛋子' + Math.random()]
         })
     }
 
-    render(){
+    render() {
         console.log('组件正在加载');
         return (
             <div>
                 <h2>一营营长 - {this.props.boss}</h2>
-                <button onClick={this.addSolder.bind(this)}>新兵入伍</button>
-                <ul>
-                    {this.state.solders.map(item=>{
-                        return <li key={item}>{item}</li>
+                <Button type="primary" onClick={this.addSolder.bind(this)}>新兵入伍</Button>
+
+                <List
+                    renderHeader='士兵列表'
+                >
+                    {this.state.solders.map(v=>{
+                        return (
+                            <List.Item
+                                key={v}
+                            >
+                                {v}
+                            </List.Item>
+                        )
                     })}
-                </ul>
+                </List>
             </div>
         )
     }
